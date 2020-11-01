24a528643a3ae5024513bc67c5cb2f78fb5e7cd2
Daisekkei <58997423+Daisekkei@users.noreply.github.com>
2020-11-02 08:50:27 +0900

    ログイン画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
   resources :posts
 
-  root to: 'posts#index'
+  root to: redirect('users/sign_in')
+  
   get 'index', to: 'posts#index'
   get 'posts', to: 'posts#index'
   get 'posts/new', to: 'posts#new'
   post'posts', to: 'posts#create'
+
 end
