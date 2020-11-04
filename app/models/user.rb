7d147439636424d3a672272f091ceabe3df2fd1e
Daisekkei <58997423+Daisekkei@users.noreply.github.com>
2020-11-04 23:11:57 +0900

    簡単ログイン機能追加、Open画面コントローラ追加” git commit -m 簡単ログイン機能追加、Open画面コントローラ追加”

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+  end
+        
 end
