9a9ed7a6fa7f87da81bbfc33f68a0484f336e147
Daisekkei <58997423+Daisekkei@users.noreply.github.com>
2021-02-03 22:50:08 +0900

    Customer登録機能の実装

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,4 +4,26 @@ class CustomersController < ApplicationController
     @customers = Customer.all
   end
 
+  def create
+    Customer.create(customer_params)
+    redirect_to controller: 'posts', action: 'index' 
+    flash[:notice] = '顧客登録しました'
+  end
+
+  def new
+    @customers = Customer.all
+    @customer = Customer.new
+
+  end
+
+
+  private
+  def customer_params
+    params.require(:customer).permit(:customer_name)
+  end
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,24 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     post '/homes/guest_sign_in', to: 'homes#new_guest'
+    
   end
   
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   
-  resources :posts
-
-  # アプリケーションを立ち上げると、瞬時に/openingsのページにアクセスされる
-  root to: redirect('openings')
+  resources :customers do
+    resources :projects
+  end
   
+  resources :projects do
+    resources :posts
+  end
+  
+
+  # アプリケーションを立ち上げると、同時に/openingsのページにアクセスされる
+  root to: redirect('openings')
   get 'openings', to: 'openings#index'
   get 'index', to: 'posts#index'
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
-  post'posts', to: 'posts#create'
   get 'users/edit', to: 'users#edit'
 
   post 'homes/guest_sign_in', to: 'posts#index'
